5bf9e0d9004385f7ddef99d981407e87e1f67e2a
Tommaso <dicamplito24@gmail.com>
2026-01-31 21:30:28 +0100

    Creazione progetto .NET MAUI in .NET 10

---
+++ App/Needy/Needy/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Needy
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ App/Needy/Needy/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Needy
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
