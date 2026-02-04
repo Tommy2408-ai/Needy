f9e6e3d6aeaf9c1bff4541c3c0a9e24e52cc9bf5
Tommaso <dicamplito24@gmail.com>
2026-02-04 18:38:01 +0100

    User class created and installation of pocetbase-csharp-sdk

---
+++ App/Needy/Needy/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Needy.Models
+{
+    public class User
+    {
+        // The unique ID that PocketBase gives to each user (e.g., "k2g4ekefa...")
+        public string Id { get; set; }
+
+        // The complete name of the user
+        public string Name { get; set; }
+
+        // Email (required for the login)
+        public string Email { get; set; }
+
+        // The avatar: contains the name of the file (ex. "foto.jpg")
+        public string Avatar { get; set; }
+
+        // True/False. If the user have permission to interact
+        public bool IsVerified { get; set; }
+        
+        // Reputation hour
+        public int ReputationHour { get; set; }
+
+        public string Neighborhood { get; set; }
+
+        public string Created {  get; set; }
+        public string Updated { get; set; }
+    }
+}
